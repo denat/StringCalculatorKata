863e5be41820554c195aa8184161475a517f83bb
denat <denat3@gmail.com>
2016-04-20 23:27:34 +0200

    Tests refactoring

--- StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -10,9 +10,7 @@ namespace StringCalculatorTests
         [TestMethod]
         public void TestAddEmptyString()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("");
+            var result = Add("");
 
             Assert.IsTrue(result == 0);
         }
@@ -20,72 +18,62 @@ namespace StringCalculatorTests
         [TestMethod]
         public void TestAddSingleNumber()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("1");
+            var result = Add("1");
             Assert.IsTrue(result == 1);
 
-            result = calculator.Add("2");
+            result = Add("2");
             Assert.IsTrue(result == 2);
         }
 
         [TestMethod]
         public void TestAddTwoNumbers()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("1,2");
+            var result = Add("1,2");
             Assert.IsTrue(result == 3);
 
-            result = calculator.Add("4,6");
+            result = Add("4,6");
             Assert.IsTrue(result == 10);
         }
 
         [TestMethod]
         public void TestAddTenNumbers()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("1,2,3,4,5,1,2,3,4,5");
+            var result = Add("1,2,3,4,5,1,2,3,4,5");
             Assert.IsTrue(result == 30);
 
-            result = calculator.Add("4,6,1,1,1,1,6,5,4,3");
+            result = Add("4,6,1,1,1,1,6,5,4,3");
             Assert.IsTrue(result == 32);
         }
 
         [TestMethod]
         public void TestAddTwoNumbersWithNewLines()
         {
-            var calculator = new Calculator();
+            var result = Add("1\n2,3");
 
-            var result = calculator.Add("1\n2,3");
             Assert.IsTrue(result == 6);
         }
 
         [TestMethod]
         public void TestAddTenNumbersWithNewLines()
         {
-            var calculator = new Calculator();
+            var result = Add("1\n2,3\n4\n5\n6,7\n8\n9\n10");
 
-            var result = calculator.Add("1\n2,3\n4\n5\n6,7\n8\n9\n10");
             Assert.IsTrue(result == 55);
         }
 
         [TestMethod]
         public void TestAddTwoNumbersWithNewDelimiter()
         {
-            var calculator = new Calculator();
+            var result = Add("//;\n5;6");
 
-            var result = calculator.Add("//;\n5;6");
             Assert.IsTrue(result == 11);
         }
 
         [TestMethod]
         public void TestAddTenNumbersWithNewDelimiter()
         {
-            var calculator = new Calculator();
+            var result = Add("//;\n1;2;3;4;5;6;7;8;9;10");
 
-            var result = calculator.Add("//;\n1;2;3;4;5;6;7;8;9;10");
             Assert.IsTrue(result == 55);
         }
 
@@ -93,63 +81,59 @@ namespace StringCalculatorTests
         [ExpectedException(typeof(Exception))]
         public void TestAddTwoNumbersSingleNegativeNumber()
         {
-            var calculator = new Calculator();
-
-            calculator.Add("5,-1");
+            Add("5,-1");
         }
 
         [TestMethod]
         [ExpectedException(typeof(Exception))]
         public void TestAddTenNumbersFiveNegativeNumbers()
         {
-            var calculator = new Calculator();
-
-            calculator.Add("//;\n1;-2;3;4;-5;6;-7;-8;9;-10");
+            Add("//;\n1;-2;3;4;-5;6;-7;-8;9;-10");
         }
 
         [TestMethod]
         public void TestIgnoreNumbersOver1000()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("5,1001");
+            var result = Add("5,1001");
             Assert.IsTrue(result == 5);
 
-            result = calculator.Add("2,2000,5,1001");
+            result = Add("2,2000,5,1001");
             Assert.IsTrue(result == 7);
 
-            result = calculator.Add("1000,1000,1001");
+            result = Add("1000,1000,1001");
             Assert.IsTrue(result == 2000);
         }
 
         [TestMethod]
         public void TestDelimitersOfAnyLength()
         {
-            var calculator = new Calculator();
-
-            var result = calculator.Add("//[***]\n1***2***3");
+            var result = Add("//[***]\n1***2***3");
             Assert.IsTrue(result == 6);
 
-            result = calculator.Add("//[^%$^%%]\n5^%$^%%6^%$^%%7");
+            result = Add("//[^%$^%%]\n5^%$^%%6^%$^%%7");
             Assert.IsTrue(result == 18);
         }
 
         [TestMethod]
         public void TestMultipleNewDelimiters()
         {
-            var calculator = new Calculator();
+            var result = Add("//[%][#]\n1#2%3");
 
-            var result = calculator.Add("//[%][#]\n1#2%3");
             Assert.IsTrue(result == 6);
         }
 
         [TestMethod]
         public void TestMultipleNewDelimitersOfAnySize()
         {
+            var result = Add("//[%%%][###]\n1###2%%%3");
+
+            Assert.IsTrue(result == 6);
+        }
+
+        private int Add(string input) {
             var calculator = new Calculator();
 
-            var result = calculator.Add("//[%%%][###]\n1###2%%%3");
-            Assert.IsTrue(result == 6);
+            return calculator.Add(input);
         }
     }
 }
